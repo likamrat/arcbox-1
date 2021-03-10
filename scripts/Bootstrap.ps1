261acebd385cd14a3d9869ac76f426034b651fc5
dkirby-ms <saitcho@outlook.com>
2021-03-10 06:19:26 -0600

    update bootstrap

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -1,30 +1,71 @@
-# Inject environment variables from parameters
 param (
-    [string]$adminUsername
+    [string]$adminUsername,
+    [string]$resourceGroup,
+    [string]$spnClientId,
+    [string]$spnClientSecret,        
+    [string]$spnTenantId,
+    [string]$Azurelocation,
+    [string]$subscriptionId
 )
 
 [System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnClientId', $spnClientId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnClientSecret', $spnClientSecret,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnTenantId', $spnTenantId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('Azurelocation', $Azurelocation,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId,[System.EnvironmentVariableTarget]::Machine)
 
 # Create path
-Write-Output "Create path"
+Write-Output "Create ArcBox path"
 $ArcBoxDir = "C:\ArcBox"
+$vmDir = "C:\ArcBox\Virtual Machines"
+$agentScript = "C:\ArcBox\agentScript"
+$tempDir = "C:\Temp"
 New-Item -Path $ArcBoxDir -ItemType directory -Force
+New-Item -Path $vmDir -ItemType directory -Force
+New-Item -Path $tempDir -ItemType directory -Force
+New-Item -Path $agentScript -ItemType directory -Force
 
 Start-Transcript "C:\ArcBox\Bootstrap.log"
 
 $ErrorActionPreference = 'SilentlyContinue'
 
+# $sourceFolder = "https://arcinbox.blob.core.windows.net/vhds"
+
 # Uninstall Internet Explorer
 Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart
 
+# Disabling IE Enhanced Security Configuration
+Write-Host "Disabling IE Enhanced Security Configuration"
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+# Extending C:\ partition to the maximum size
+Write-Host "Extending C:\ partition to the maximum size"
+Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
+
+# Installing DHCP service 
+Write-Output "Installing DHCP service"
+Install-WindowsFeature -Name "DHCP" -IncludeManagementTools
+
 # Install Windows Subsystem for Linux
 # Used for Bash shell to SSH to the ArcBoxUbuntu
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
 
 # Installing tools
 workflow ClientTools_01
         {
-            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli,vcredist140,azcopy10,microsoft-edge,vscode,git,7zip,kind'
+            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,git,7zip,kubectx,terraform,putty.install'
             #Run commands in parallel.
             Parallel 
                 {
@@ -57,10 +98,12 @@ workflow ClientTools_01
                     # Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/settings.json" -OutFile "C:\tmp\settings.json"
                     # Invoke-WebRequest "https://aka.ms/azdata-msi" -OutFile "C:\tmp\AZDataCLI.msi"
                     Invoke-WebRequest "https://arcinbox.blob.core.windows.net/scripts/LogonScript.ps1" -OutFile "C:\ArcBox\LogonScript.ps1"
+                    Invoke-WebRequest "https://arcinbox.blob.core.windows.net/scripts/installArcAgent.ps1" -OutFile "C:\ArcBox\agentScript\installArcAgent.ps1"
                 }
         }
 
 ClientTools_01 | Format-Table
+RefreshEnv
 
 # workflow ClientTools_02
 #         {
@@ -76,10 +119,17 @@ ClientTools_01 | Format-Table
         
 # ClientTools_02 | Format-Table 
 
+
+
+# Clonning the Azure Arc Jumpstart git repository
+Write-Output "Clonning the Azure Arc Jumpstart git repository"
+git clone https://github.com/microsoft/azure_arc.git "C:\ArcBox\azure_arc"
+
 # New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
 # New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
 
-# Downloading the LogonScript and creating Scheduled Task for it
+
+# Downloading the LogonScript and creating scheduled task
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\ArcBox\LogonScript.ps1'
 Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User $adminUsername -Action $Action -RunLevel "Highest" -Force
@@ -89,4 +139,6 @@ Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
 
 # Install Hyper-V and reboot
 Write-Host "Installing Hyper-V and restart"
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+Install-WindowsFeature -Name Hyper-V -IncludeAllSubFeature -IncludeManagementTools -Restart
