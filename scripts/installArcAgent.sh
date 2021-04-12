da678b3e0dbc5b1df40cfa8e14f801c560cad428
likamrat <likamrat@microsoft.com>
2021-04-11 19:17:10 -0700

    ubuntu fix

--- scripts/installArcAgent.sh
+++ scripts/installArcAgent.sh
@@ -3,10 +3,10 @@
 apt-get update
 
 # Download the installation package
-wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh exec 2> /dev/null
+wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh 2>/dev/null
 
 # Install the hybrid agent
-bash ~/install_linux_azcmagent.sh exec 2> /dev/null
+bash ~/install_linux_azcmagent.sh 2>/dev/null
 
 # Run connect command
 azcmagent connect --service-principal-id $spnClientId --service-principal-secret $spnClientSecret --resource-group $resourceGroup --tenant-id $spnTenantId --location $Azurelocation --subscription-id $subscriptionId --resource-name "ArcBoxUbuntu" --cloud "AzureCloud" --tags "Project=jumpstart_arcbox"
